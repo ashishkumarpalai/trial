d78cd19414fdda206b4e1bf210597c76af554de7
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2023-11-25 19:38:18 +0530

    ok

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
